d565ee52c771a685c86d6cf835ba682fa1bf4f4b
majidaldo <majidaldosar-github@yahoo.com>
2014-06-10 13:38:52 -0400

    update paths in scraper config

--- scrape/scrape_config.py
+++ scrape/scrape_config.py
@@ -3,8 +3,8 @@ browser_drivers=dict.fromkeys(browsers)
 default_browser='chrome'
 
 #add/change your driver here
-browser_drivers['chrome']=r'c:\users\majid\desktop\chromedriver.exe'
+browser_drivers['chrome']=r'C:\Users\Majid\Documents\transportation\data\scraped\drivers\chromedriverw32.exe'
 
 browser_download_dir=r'C:\Users\Majid\Downloads'
-extract_to=r'C:\Users\Majid\Dropbox\Transportation\data\scraped\bts'
+extract_to=r'C:\Users\Majid\Documents\transportation\data\scraped\bts'
 
