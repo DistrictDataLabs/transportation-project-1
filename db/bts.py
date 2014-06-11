b49013d9e2b5d846472c5a90e9df3b0d8c975efb
majidaldo <majidaldosar-github@yahoo.com>
2014-06-11 15:04:02 -0400

    init db stuff

--- db/bts.py
+++ db/bts.py
@@ -1,17 +1,17 @@
-#go thru dirs of varsetunique
-#take a data file csv in it and take header line to make a table
-#import the data to the table
 
-# CSV Import
+from resources import dirs
+import os
 
-# Use the ".import" command to import CSV (comma separated value) data into an SQLite table. The ".import" command takes two arguments which are the name of the disk file from which CSV data is to be read and the name of the SQLite table into which the CSV data is to be inserted.
+def loadDBpth():return os.path.join(dirs['data_store'],'data/scraped/bts/load.sqlite') 
 
-# Note that it is important to set the "mode" to "csv" before running the ".import" command. This is necessary to prevent the command-line shell from trying to interpret the input file text as some other format.
+def locate_loadDB():
+    """finds path of load db..returns None if not found"""
+    loc= loadDBpth()
+    if os.path.exists(loc)==True: return loc
+    else: return None
 
-# sqlite> .mode csv
-# sqlite> .import C:/work/somedata.csv tab1
-# There are two cases to consider: (1) Table "tab1" does not previously exist and (2) table "tab1" does already exist.
+load_dbpth=locate_loadDB()
 
-# In the first case, when the table does not previously exist, the table is automatically created and the content of the first row of the input CSV file is used to determine the name of all the columns in the table. In other words, if the table does not previously exist, the first row of the CSV file is interpreted to be column names and the actual data starts on the second row of the CSV file.
-
-# For the second case, when the table already exists, every row of the CSV file, including the first row, is assumed to be actual content. If the CSV file contains an initial row of column labels, that row will be read as data and inserted into the table. To avoid this, make sure that table does not previously exist.
+def create_loadDB(overwrite=False):
+    pass
+    
