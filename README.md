333307fed936d76badc06955bd511c73013f7369
majidaldo <majidaldosar-github@yahoo.com>
2014-07-29 22:36:02 -0400

    changed data dir
    ..it's simpler this way.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 setup
 =====
-1. shared folder. (and look at rootfolder.md)
-2. 
+
+data dir is shared across members. 
+
+1. get data dir (using bittorent sync for example)
+2. set data_dir in project src. probably best to just make it a subfolder as is so in the source
+3. make the data_dir the shared folder. copy it in its place or link to it.
--- resources.py
+++ resources.py
@@ -4,9 +4,11 @@ locations of resources
 
 import os
 
+data_dir='data_store'
+
 #put data store so that it is one level up from the code repo
 dirs={
-    'data_store':os.path.join(os.path.dirname( __file__ ), '..')
+    'data_store':os.path.join(os.path.dirname( __file__ ), data_dir)
     ,
     'code': os.path.join(os.path.dirname( __file__ ))
     }
