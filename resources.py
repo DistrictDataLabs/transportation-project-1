b633f956def8237eaac33a92eb4b03a12015426b
majidaldo <majidaldosari-github@yahoo.com>
2014-08-08 02:21:24 -0400

    too slow

--- resources.py
+++ resources.py
@@ -12,11 +12,16 @@ dirs={
     ,
     'code': os.path.join(os.path.dirname( __file__ ))
     }
+files={'bts_sqlt3': os.path.join(dirs['data_store']
+                                 ,'data','scraped','bts','load.sqlite3')
+                            }
 
 def __init__(abs_path=True):
     
     for arsrc in dirs:
         dirs[arsrc]=os.path.abspath(dirs[arsrc])
+    for arsrc in files:
+        files[arsrc]=os.path.abspath(files[arsrc])
 
     
 __init__()
